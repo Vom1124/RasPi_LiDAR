3fcad02adea457e8ed99fd31b29e2275ca56a177
Vom <129146507+Vom1124@users.noreply.github.com>
2023-08-26 01:58:24 -0500

    Update lidar_sub.py

--- src/velodyne_lidar/velodyne_lidar/lidar_sub.py
+++ src/velodyne_lidar/velodyne_lidar/lidar_sub.py
@@ -7,49 +7,57 @@ from sensor_msgs.msg import PointCloud2
 from laser_geometry import LaserProjection
 import numpy as np
 
+fd = False
 
 class LiDAR(Node):
     
     def __init__(self):
         
         super().__init__('PointCloud')
+        self.counter=0
+        self.subscribeRawData = self.create_subscription(VelodyneScan, '/velodyne_packets', self.raw_callback, 10)
+        head_txt = "\nPoint Cloud Generation Initiated\n"
+        fmt_head_txt = head_txt.center(150,'=')
+        print("\033[36:4m" + fmt_head_txt)
         
-        #self.publishPointCloud = self.create_publisher(PointCloud2, '/velodyne_points', 10 )
-        #self.subscribeRawData = self.create_subscription(VelodyneScan, '/velodyne_packets', self.raw_callback)
-        t=0
-        with open("PC.txt", mode="wt") as self.f:
-            self.f.truncate(0)
-            self.f.write(" %f times, %([t+1]")
-            
         self.subscribePointCloud = self.create_subscription(PointCloud2, '/velodyne_points', self.pc_callback, 10)
-        self.get_logger().info("Point Cloud Generation Initiated")
-        
+                
         self.subscribePointCloud # preventing unused variable
         
         self.laser_projector = LaserProjection()
 
     def raw_callback(self, data):
-        i = 0
-        while True:
-            self.get_logger().info("Receiving Raw Data\n")
-            if i==0:
-                print(data)
-                i+=1
+
+        if self.counter==0:
+            raw_data_txt = "Printing Raw Data Packets (This will be displayed only once every time the code is run).\n"
+            raw_data_txt_conf = "Finished receiving the raw data packets\n\n"
+            self.get_logger().info("\033[93:4m" + raw_data_txt)
+            print(data)    
+            self.get_logger().info("\033[92:4m" + raw_data_txt_conf)        
+            
     def pc_callback(self, pc):
-        self.get_logger().info("Receiving point cloud points\n")
+        pc_txt = "Receiving point cloud points and saving ...\n"
         time.sleep(2)
-        with open("PC.txt", mode="wt") as self.f:
-            self.f.write(str(pc))
-            self.f.write("\n\n\n\ntimes000000000000000000000000000...........................................................\n\n\n\n")
-        
-        self.f.flush
-        self.f.close
+        self.counter+=1
+        fd.write(str(pc))
+        fd.write("\n\n\n\n ======================================End of one spin:{}===================================\n\n\n".format(self.counter))
+        self.get_logger().info("\033[95m" + pc_txt)
         
 def main(args=None):
     rclpy.init(args=args)
     node = LiDAR()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    global fd
+
+    fd = open("pc_output.txt","w")
+    
+    try:
+        rclpy.spin(node)
+    
+    finally:
+        rclpy.shutdown()
+        fd.close()
+        
+    
 
     
 if __name__=='__main__':
